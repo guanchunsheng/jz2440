2391a15b670b20daa90a6111fe7eb64d07097426
guanchunsheng <guanchunsheng@163.com>
2017-08-13 18:56:28 +0800

    extend c code to make led lights as binary counter.

--- led/led_on_c/led_on_c.c
+++ led/led_on_c/led_on_c.c
@@ -1,12 +1,19 @@
 #define GPFCON		(*(volatile unsigned long *)0x56000050)
 #define GPFDAT		(*(volatile unsigned long *)0x56000054)
 #define GPF_OUT(x)	(1 << (2 * (x)))
-#define GPF_SET(x)	(1 << (x))
+#define LED_NUM(x)  (~(((x) & ~((~0) << 3)) << 4))
 
 int main()
 {
-	/* 设置GPF4 5 6为输出端口，GPF4 GPF6为高电平，LED2亮，LED1和3不亮*/
-	GPFCON = GPF_OUT(4) | GPF_OUT(5) | GPF_OUT(6);
-	GPFDAT = GPF_SET(4) | GPF_SET(6);
-	return 0;
+    unsigned int i;
+    unsigned long j;
+    /* led to count, in binary */
+    GPFCON = GPF_OUT(4) | GPF_OUT(5) | GPF_OUT(6);
+    for (i = 0;;i = ++i % 8)
+    {
+        GPFDAT = LED_NUM(i);
+        for (j = 0; j < 100000; j++)
+            ;
+    }
+    return 0;
 }
